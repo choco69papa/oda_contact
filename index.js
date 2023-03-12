2410cb5d10e2729407e39d834f01f86a1bb21948
choco69papa <122781148+choco69papa@users.noreply.github.com>
2023-03-12 18:11:49 +0900

    Update index.js

--- index.js
+++ index.js
@@ -3,16 +3,16 @@ $(function () {
     // お問い合わせフォームを表示する
     $('#form-number').click(function () {
         $('#form-name').empty();
-	var numbe = $('input[name="numbe"]').val();
+	var namelabel = $('input[name="namelabel"]').val();
         var inquiries = $('textarea[name="inquiries"]').val();
     });
 
     // 送信
     $('form').submit(function () {
-	var numbe = $('input[name="numbe"]').val();
+	var namelabel = $('input[name="namelabel"]').val();
         var inquiries = $('textarea[name="inquiries"]').val();
 	    
-        var msg = `＊＊お問い合わせ内容＊＊\n ${numbe}\n ${inquiries}`;
+        var msg = `＊＊お問い合わせ内容＊＊\n ${namelabel}\n ${inquiries}`;
         sendText(msg);
 
         return false;
